4171b8ad7348e5040c2e1bd88f0017230158bcbe
OlaAdams <adamsonoladipupo>
2026-03-25 17:03:03 +0100

    new update

---
+++ MySQL Project/chapter two/class tasks/confirm_stored_data.sql
@@ -0,0 +1,9 @@
+use classic_models_db;
+select * from Offices;
+select * from Employees;
+select * from ProductLines;
+select * from Products;
+select * from Customers;
+select * from OrderDetails;
+select * from Orders;
+select * from Payments;
